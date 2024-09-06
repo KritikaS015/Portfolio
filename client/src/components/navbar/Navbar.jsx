b89a3d83397506ca2236486fac5fb8dad5252091
KritikaS015 <kritika8939g@gmil.com>
2024-09-06 23:42:47 +0530

    Navbar done

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,25 +3,30 @@ import { Link } from "react-scroll";
 
 export default function Navbar(props) {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  // Declare md as a variable
+  const md = window.matchMedia("(min-width: 768px)");
 
   return (
-    <nav className="flex justify-between items-center h-[8vh] bg-[#001f3f] sticky top-0 z-10">
+    <nav className="flex justify-between items-center h-[8vh] bg-black sticky top-0 z-10">
       <div className="flex items-center h-full px-4 md:px-8 lg:px-12">
         <i
           className="bi bi-list text-white text-2xl font-bold cursor-pointer md:hidden"
           onClick={toggleMenu}
         ></i>
-        <span className="ml-4 text-white font-bold text-2xl md:text-4xl ">
+        <span className="ml-4 text-white font-bold text-[30px] sm:text-4xl md:text-4xl ">
           {props.title}
         </span>
       </div>
       <div
-        className={`md:flex md:items-center md:space-x-8 ${
-          isMenuOpen ? "block" : "hidden"
-        }`}
+        className={md.matches ? "flex md:items-center md:space-x-8" : "hidden"}
       >
         <ul className="flex flex-col md:flex-row space-x-8 cursor-pointer">
           <li className="relative group">
@@ -83,6 +88,52 @@ export default function Navbar(props) {
               <span className="absolute -bottom-1 left-0 w-full h-1 bg-gradient-to-r from-green-400 to-blue-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
               <span className="absolute -top-[0.5] left-0 w-full h-1 bg-gradient-to-r from-green-400 to-blue-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
             </Link>
+          </li>
+          <li className="relative group">
+            <button
+              onClick={toggleDropdown}
+              className="text-white text-lg font-semibold capitalize relative mr-8"
+            >
+              More
+              <span className="absolute -bottom-1 left-0 w-full h-1 bg-gradient-to-r from-green-400 to-blue-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+              <span className="absolute -top-[0.5] left-0 w-full h-1 bg-gradient-to-r from-green-400 to-blue-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+            </button>
+            <ul
+              className={`absolute top-full left-0 bg-black p-4 rounded-md ${
+                isDropdownOpen ? "block" : "hidden"
+              }`}
+            >
+              <li className="relative group">
+                <Link
+                  to="projects"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={10}
+                  onClick={toggleDropdown}
+                  className="text-white text-lg font-semibold capitalize relative"
+                >
+                  Projects
+                  <span className="absolute -bottom-1 left-0 w-full h-1 bg-gradient-to-r from-green-400 to-blue-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+                  <span className="absolute -top-[0.5] left-0 w-full h-1 bg-gradient-to-r from-green-400 to-blue-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+                </Link>
+              </li>
+              <li className="relative group">
+                <Link
+                  to="blog"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={10}
+                  onClick={toggleDropdown}
+                  className="text-white text-lg font-semibold capitalize relative"
+                >
+                  Blog
+                  <span className="absolute -bottom-1 left-0 w-full h-1 bg-gradient-to-r from-green-400 to-blue-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+                  <span className="absolute -top-[0.5] left-0 w-full h-1 bg-gradient-to-r from-green-400 to-blue-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+                </Link>
+              </li>
+            </ul>
           </li>
         </ul>
       </div>
